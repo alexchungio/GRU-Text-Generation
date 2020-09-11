b8b484606616eb06cf88b26e6b112f9cfb37c65d
alexchung <yonganzhong@outlook.com>
2020-09-11 14:40:39 +0800

    update train module

--- train.py
+++ train.py
@@ -12,22 +12,13 @@
 
 
 import os
-import time
-import json
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 
 from libs.configs import cfgs
-from libs.nets.model import build_model
-
-
-def split_input_target(chunk):
-    input_text = chunk[:-1]
-    target_text = chunk[1:]
-    return input_text, target_text
-
-def loss(labels, logits):
-    return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+from libs.nets.model import GRU
+from data.dataset_pipeline import load_parse_data, dataset_batch
 
 
 if __name__ == "__main__":
@@ -55,61 +46,15 @@ if __name__ == "__main__":
     #     logical_devices = [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]
     # )
 
+    # load dataset
+    text, vocab, char_index, index_char = load_parse_data(cfgs.DATASET_PATH)
 
-    # -----------------------download dataset------------------------------------
-    file_path = tf.keras.utils.get_file(cfgs.DATASET_PATH,
-                                        origin='https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-
-    # ----------------------read and decode data------------------------------------
-    with open(file_path, 'rb') as f:
-        text = f.read().decode(encoding='utf-8')
-
-    vocab = sorted(set(text))
-    print('{} unique characters'.format(len(vocab)))
-
-    # get and save char index
-    char_index = {char: index for index, char in enumerate(vocab)}
-    index_char = np.array(vocab)
-
-    with open(cfgs.CHAR_INDEX, 'w') as f:
-        f.write(json.dumps(char_index))
-
-    # map strings to a numerical representation
-    text_sequence = np.array([char_index[char] for char in text])
-
-    seq_length = 100
     examples_per_epoch = len(text)
-    char_dataset = tf.data.Dataset.from_tensor_slices(text_sequence)
 
-    sequences = char_dataset.batch(seq_length, drop_remainder=True)
-
-
-    # use map apply method to each batch
-    dataset = sequences.map(split_input_target)
-
-    # for input_example, target_example in dataset.take(5):
-    #     print("Input data: ", repr(''.join(index_char[input_example.numpy()])))
-    #     print("target data: ", repr(''.join(index_char[target_example.numpy()])))
-
-    # data config
-    BATCH_SIZE = 64
-    BUFFER_SIZE = 10000
-    dataset = dataset.shuffle(buffer_size=BUFFER_SIZE).batch(batch_size=BATCH_SIZE, drop_remainder=True)
-
-    # for input_example, target_example in dataset.take(5):
-    #     print("Input data: ", repr(''.join(index_char[input_example.numpy()[0]])))
-    #     print("target data: ", repr(''.join(index_char[target_example.numpy()[0]])))
+    dataset = dataset_batch(text, char_index=char_index, seq_length=cfgs.SEQUENCE_LENGTH, batch_size=cfgs.BATCH_SIZE)
 
     # ---------------------------------construct network----------------------
-    # length of vocab_size
-    vocab_size = len(vocab)
-    # embedding dimension
-    embedding_dim = 256
-    # rnn units
-    num_units = 1024
-
-    model = build_model(vocab_size, embedding_dim, num_units, batch_size=BATCH_SIZE)
-
+    model = GRU(vocab_size=len(vocab), embedding_size=cfgs.EMBEDDING_DIM, num_units=cfgs.NUM_UNITS)
     # print(model.summary())
 
     # predict
@@ -129,61 +74,68 @@ if __name__ == "__main__":
     #     example_batch_loss = loss(target_example_batch, example_batch_predictions)
     #     print(example_batch_loss.shape)
 
-    # Define our metrics
-    train_loss = tf.keras.metrics.Mean('train_loss', dtype=tf.float32)
-    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy('train_accuracy')
+    saver = tf.train.Saver(max_to_keep=30)
 
-    summary_writer = tf.summary.create_file_writer(cfgs.SUMMARY_PATH)
+    # get computer graph
+    graph = tf.get_default_graph()
 
-    optimizer = tf.keras.optimizers.Adam()
+    write = tf.summary.FileWriter(logdir=cfgs.SUMMARY_PATH, graph=graph)
 
-    @tf.function
-    def train_step(input, target):
-        with tf.GradientTape() as tape:
-            predictions = model(input)
-            loss = tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(
-                target, predictions, from_logits=True))
-        grads = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
-        train_loss(loss)
+    # os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+    config = tf.ConfigProto()
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # maximun alloc gpu50% of MEM
+    config.gpu_options.allow_growth = True
 
-        return loss
+    init_op = tf.group(
+        tf.global_variables_initializer(),
+        tf.local_variables_initializer()
+    )
+    # train and save model
+    with tf.Session(config=config) as sess:
+        sess.run(init_op)
 
-    # name of checkpoint files
-    checkpoint_prefix = os.path.join(cfgs.TRAINED_CKPT, "ckpt_{epoch}")
+        # get model variable of network
+        model_variable = tf.model_variables()
+        for var in model_variable:
+            print(var.op.name, var.shape)
 
-    # checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix, save_weights_only=True)
+        # ------------------load embedding pretrained weights---------------------
+        # parse glove pretrained model
+        # -----------------------train part------------------------------------------------
+        # merges all summaries collected in the default graph
+        summary_op = tf.summary.merge_all()
 
-    # EPOCHS= 10
-    # history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
-    for epoch in range(cfgs.NUM_EPOCH):
+        train_step_per_epoch = examples_per_epoch // cfgs.BATCH_SIZE
 
-        start = time.time()
-        # resetting the hidden state at the start of every epoch
-        # hide state is None at start
-        hidden = model.reset_states()
+        # generate batch
+        train_data_batch, train_label_batch = dataset.get_next()
 
-        for num_step, (input, target) in enumerate(dataset):
-            loss = train_step(input, target)
+        # use k folder validation
+        for epoch in range(cfgs.NUM_EPOCH):
+            train_bar = tqdm(range(1, train_step_per_epoch + 1))
+            train_loss = []
+            for step in train_bar:
+                x_train, y_train = sess.run([train_data_batch, train_label_batch])
+                feed_dict = model.fill_feed_dict(x_train, y_train, keep_prob=cfgs.KEEP_PROB)
+                summary, global_step, loss, _ = sess.run(
+                    [summary_op, model.global_step, model.loss, model.train],
+                    feed_dict=feed_dict)
+                train_loss.append(loss)
+                if step % cfgs.SMRY_ITER == 0:
+                    write.add_summary(summary=summary, global_step=global_step)
+                    write.flush()
 
-            if (num_step+1) % cfgs.SHOW_TRAIN_INFO_INTE == 0:
-                print('Epoch {} Step {} Loss {}'.format(epoch+1, num_step, loss))
+                train_bar.set_description("Epoch {0} : Step {1} => Train Loss: {2:.4f} ".
+                                          format(epoch + 1, step, train_loss))
 
-        with summary_writer.as_default():
-            tf.summary.scalar('loss', train_loss.result(), step=epoch)
-            # tf.summary.scalar('accuracy', train_accuracy.result(), step=epoch)
+            # save model
+            ckpt_file = os.path.join(cfgs.TRAINED_CKPT, 'model_loss={0:4f}.ckpt'.format(sum(train_loss) / len(train_loss)))
+            if epoch % cfgs.SAVE_WEIGHTS_ITER == 0:
+                saver.save(sess=sess, save_path=ckpt_file, global_step=global_step)
+        saver.save(sess=sess, save_path=ckpt_file, global_step=global_step)
+    sess.close()
+    print('model training has complete')
 
-        # save checkpoint
-        if (epoch+1) % cfgs.SAVE_WEIGHTS_INER == 0:
-            model.save_weights(checkpoint_prefix.format(epoch=epoch))
-        print('Epoch {} train Loss {}'.format(epoch+1, train_loss.result()))
-        print ('Time taken for epoch {} sec\n'.format(time.time() - start))
-
-        # Reset metrics every epoch
-        train_loss.reset_states()
-        train_accuracy.reset_states()
-
-    model.save_weights(checkpoint_prefix.format(epoch=cfgs.NUM_EPOCH))
 
 
 
